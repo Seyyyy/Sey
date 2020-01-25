da19528221fc909b12b9b6bc9ce831455efc4d2b
shoshiro-furube <mysdc7@gmail.com>
2020-01-26 01:21:40 +0900

    add linkbuttom

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
+import Fab from '@material-ui/core/Fab'
 import KyoryuIcon from '@material-ui/core/SvgIcon'
 import KyoryuSVG from '../../static/icons/kyoryu.svg'
 import { makeStyles } from '@material-ui/core/styles'
@@ -13,21 +15,54 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     justifyContent: 'center',
   },
-  title: {
-    margin: '0 auto',
-    marginTop: '30px',
-    fontSize: '100px',
-    letterSpacing: '20px',
-    textIndent: '20px',
-    color: theme.palette.primary.contrastText,
-  },
   logo: {
     margin: '0 auto',
     width: '30vw',
     height: '30vh',
     color: theme.palette.primary.contrastText,
   },
+  title: {
+    margin: '0 auto',
+    marginTop: '30px',
+    marginBottom: '30px',
+    fontSize: '100px',
+    letterSpacing: '20px',
+    textIndent: '20px',
+    color: theme.palette.primary.contrastText,
+  },
+  link: {
+    margin: '0 auto',
+    textDecoration: 'none',
+  },
+  button: {
+    color: theme.palette.primary.main,
+    size: 'large',
+    backgroundColor: theme.palette.primary.contrastText,
+  },
 })
+
+const Kyoryu = () => {
+  const classes = useStyles()
+  return (
+    <KyoryuIcon
+      className={classes.logo}
+      component={KyoryuSVG}
+      viewBox={'0 0 94.89 76.39'}
+    />
+    /* svgアイコンのviewboxと同値に設定 */
+  )
+}
+
+const Work = () => {
+  const classes = useStyles()
+  return (
+    <RouterLink className={classes.link} to="/work">
+      <Fab className={classes.button} variant={'extended'}>
+        work
+      </Fab>
+    </RouterLink>
+  )
+}
 
 const Home = () => {
   const classes = useStyles()
@@ -35,14 +70,9 @@ const Home = () => {
   return (
     <React.Fragment>
       <Grid className={classes.root}>
-        <KyoryuIcon
-          className={classes.logo}
-          component={KyoryuSVG}
-          viewBox={'0 0 94.89 76.39'}
-        />
-        {/* svgアイコンのviewboxと同値に設定 */}
+        <Kyoryu />
         <Grid className={classes.title}>Sey</Grid>
-        <Grid></Grid>
+        <Work />
         <Grid></Grid>
       </Grid>
     </React.Fragment>
