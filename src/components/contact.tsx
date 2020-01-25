7bb99ab10636e9c3ec0022317ecc5ce2331ad7cc
shoshiro-furube <mysdc7@gmail.com>
2020-01-26 05:37:43 +0900

    add contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core'
+import theme from '../GlobalTheme'
+
+const useStyles = makeStyles({
+  root: {
+    width: '100vw',
+    height: 'auto',
+    paddingTop: '80px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  container: {
+    margin: '0 auto',
+    marginTop: '20px',
+    marginBottom: '20px',
+  },
+  subTitle: {
+    color: theme.palette.primary.contrastText,
+    fontSize: theme.typography.h4.fontSize,
+    fontWeight: theme.typography.h4.fontWeight,
+    textTransform: 'uppercase',
+    marginBottom: '80px',
+  },
+  heading: {
+    color: theme.palette.primary.contrastText,
+    fontSize: theme.typography.h5.fontSize,
+    fontWeight: theme.typography.h5.fontWeight,
+  },
+  paragraph: {
+    color: theme.palette.primary.contrastText,
+    fontSize: theme.typography.fontSize,
+  },
+})
+
+const SubTitle = () => {
+  const classes = useStyles()
+  return (
+    <Grid className={classes.container}>
+      <Grid className={classes.subTitle}>Contact</Grid>
+    </Grid>
+  )
+}
 
 const Contact = () => {
+  const classes = useStyles()
+
   return (
     <React.Fragment>
-      <div>contact</div>
+      <Grid className={classes.root}>
+        <SubTitle />
+      </Grid>
     </React.Fragment>
   )
 }
