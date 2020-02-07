ff3ffd166544d2beb8548c746a2b35a933ab878b
Seyyyy <mysdc7@gmail.com>
2020-02-08 05:50:03 +0900

    fix contact typo

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,7 +4,7 @@ import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core'
 import theme from '../GlobalTheme'
 import Input from '@material-ui/core/Input'
-import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles({
   root: {
@@ -22,21 +22,20 @@ const useStyles = makeStyles({
   },
   subTitle: {
     color: theme.palette.primary.contrastText,
-    fontSize: theme.typography.h4.fontSize,
-    fontWeight: theme.typography.h4.fontWeight,
     textTransform: 'uppercase',
     marginBottom: '80px',
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '20px',
+    },
   },
   heading: {
     color: theme.palette.primary.contrastText,
-    fontSize: theme.typography.h5.fontSize,
-    fontWeight: theme.typography.h5.fontWeight,
   },
   paragraph: {
     color: theme.palette.primary.contrastText,
-    fontSize: theme.typography.fontSize,
-    marginRight: '10px',
-    marginLeft: '10px',
+    marginRight: '25px',
+    marginLeft: '25px',
+    marginBottom: '10px',
   },
   textField: {
     width: '400px',
@@ -50,7 +49,9 @@ const SubTitle = () => {
   const classes = useStyles()
   return (
     <Grid className={classes.container}>
-      <Grid className={classes.subTitle}>Contact</Grid>
+      <Typography variant="h4" className={classes.subTitle}>
+        Contact
+      </Typography>
     </Grid>
   )
 }
@@ -59,13 +60,15 @@ const Paragraph = () => {
   const classes = useStyles()
   return (
     <Grid className={classes.container}>
-      <p className={classes.paragraph}>
+      <Typography variant="body1" className={classes.paragraph}>
         ・サークルやコミュニティなどへのお誘いは大歓迎です！(喜びます)
-      </p>
-      <p className={classes.paragraph}>・TwitterでDMくださっても大丈夫です！</p>
-      <p className={classes.paragraph}>
+      </Typography>
+      <Typography variant="body1" className={classes.paragraph}>
+        ・TwitterでDMくださっても大丈夫です！
+      </Typography>
+      <Typography variant="body1" className={classes.paragraph}>
         ・お仕事やご依頼はお受けしてません。(まだ自信がない、、)
-      </p>
+      </Typography>
     </Grid>
   )
 }
