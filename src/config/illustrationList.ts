66b45f3d04eca654f52e5fdee4a1b5a52ac2675c
Seyyyy <mysdc7@gmail.com>
2020-03-01 04:26:18 +0900

    fix: image url

--- src/config/illustrationList.ts
+++ src/config/illustrationList.ts
@@ -1,12 +1,12 @@
 const illustrationList = [
   {
-    img: 'https://www.dropbox.com/s/kikzgyundylc66d/sky.jpg?dl=1',
+    img: 'https://dl.dropbox.com/s/kikzgyundylc66d/sky.jpg?dl=1',
     title: 'Sky',
     author: 'author',
     cols: 1,
   },
   {
-    img: 'https://www.dropbox.com/s/5h6xye8bcthcjnr/work.gif?dl=1',
+    img: 'https://dl.dropbox.com/s/5h6xye8bcthcjnr/work.gif?dl=1',
     title: 'Walking',
     author: 'author',
     cols: 2,
