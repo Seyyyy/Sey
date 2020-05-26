5d1b3106a8478a57cdfb5d1e9028562aea929b13
Seyyyy <mysdc7@gmail.com>
2020-05-26 20:40:21 +0900

    fix: add labels

--- src/components/Sns/index.tsx
+++ src/components/Sns/index.tsx
@@ -6,13 +6,13 @@ const Sns = () => {
   return (
     <div>
       <div className={styles.sns}>
-        <a href="https://github.com/Seyyyy">
+        <a href="https://github.com/Seyyyy" aria-label="github">
           <SvgGithub width={'40px'} height={'40px'} />
         </a>
-        <a href="https://twitter.com/seyyyysaid">
+        <a href="https://twitter.com/seyyyysaid" aria-label="twitter">
           <SvgTwitter width={'40px'} height={'40px'} />
         </a>
-        <a href="https://note.com/seyyyy">
+        <a href="https://note.com/seyyyy" aria-label="note">
           <SvgNote width={'40px'} height={'40px'} />
         </a>
       </div>
