bb0bb461d83ed416faa27feba140ce524ea39360
Seyyyy <mysdc7@gmail.com>
2020-05-26 19:17:23 +0900

    feat: footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {AppProps} from 'next/app'
+import Footer from '../components/Footer'
 import '../style/style.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <React.Fragment>
             <Component {...pageProps} />
+            <Footer />
         </React.Fragment>
     )
 }
