de3464bdb2357ce6e13c8f5ee8ff0839effc0a0f
Seyyyy <mysdc7@gmail.com>
2023-01-01 01:27:34 +0900

    warningの解決

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head />
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Sey</title>
         <meta name="description" content="Sey's Portfolio" />
