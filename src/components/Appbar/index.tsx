da795aa19c48131bef75e75d290c4d1f49d9d627
Seyyyy <mysdc7@gmail.com>
2023-01-01 04:57:16 +0900

    fix: accessibility

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -13,33 +13,35 @@ const Appbar = () => {
   }
 
   return (
-    <nav className={styles.root}>
-      <li>
-        <a href={'/'}>
-          <div className={styles.titleRoot}>
-            <SvgKyoryu width={'40px'} height={'100%'} />
-            <h1 className={styles.titleText}>Sey</h1>
-          </div>
-        </a>
-      </li>
+    <nav>
+      <ul className={styles.root}>
+        <li>
+          <a href={'/'}>
+            <div className={styles.titleRoot}>
+              <SvgKyoryu width={'40px'} height={'100%'} />
+              <h1 className={styles.titleText}>Sey</h1>
+            </div>
+          </a>
+        </li>
 
-      <li>
-        <Tooltip
-          buttonProps={{
-            'aria-label': 'Change Theme',
-            className: styles.themeToggle,
-            onClick: onChangeTheme,
-          }}
-          tootipText={'Change theme'}
-          data-testid="theme-button"
-        >
-          {theme ? (
-            <FontAwesomeIcon icon={faMoon} className={styles.icon} />
-          ) : (
-            <FontAwesomeIcon icon={faSun} className={styles.icon} />
-          )}
-        </Tooltip>
-      </li>
+        <li>
+          <Tooltip
+            buttonProps={{
+              'aria-label': 'Change Theme',
+              className: styles.themeToggle,
+              onClick: onChangeTheme,
+            }}
+            tootipText={'Change theme'}
+            data-testid="theme-button"
+          >
+            {theme ? (
+              <FontAwesomeIcon icon={faMoon} className={styles.icon} />
+            ) : (
+              <FontAwesomeIcon icon={faSun} className={styles.icon} />
+            )}
+          </Tooltip>
+        </li>
+      </ul>
     </nav>
   )
 }
