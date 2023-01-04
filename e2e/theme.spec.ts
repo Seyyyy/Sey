e9142dc5538d867cf392b2b2ab36adb46cb69041
Seyyyy <mysdc7@gmail.com>
2023-01-04 10:08:15 +0900

    vrt追加

--- e2e/theme.spec.ts
+++ e2e/theme.spec.ts
@@ -6,8 +6,13 @@ test.describe('UIのテーマをダークモードとライトモードにスイ
   }) => {
     await page.goto('http://localhost:3000/')
     await page.getByRole('button', { name: 'Change Theme' }).click()
-    const result = await page.locator('html.dark').all()
-    await expect(result.length).toBe(1)
+    // const result = await page.locator('html.dark').all()
+    // await expect(result.length).toBe(1)
+    expect(
+      await page.screenshot({ animations: 'disabled', fullPage: true })
+    ).toMatchSnapshot('darkmode.png', {
+      threshold: 0.1,
+    })
   })
 
   test('ナビゲーションバーのテーマチェンジボタンを押下するとライトモードが表示される', async ({
@@ -16,7 +21,12 @@ test.describe('UIのテーマをダークモードとライトモードにスイ
     await page.goto('http://localhost:3000/')
     await page.getByRole('button', { name: 'Change Theme' }).click()
     await page.getByRole('button', { name: 'Change Theme' }).click()
-    const result = await page.locator('html.light').all()
-    await expect(result.length).toBe(1)
+    // const result = await page.locator('html.light').all()
+    // await expect(result.length).toBe(1)
+    expect(
+      await page.screenshot({ animations: 'disabled', fullPage: true })
+    ).toMatchSnapshot('lightmode.png', {
+      threshold: 0.1,
+    })
   })
 })
