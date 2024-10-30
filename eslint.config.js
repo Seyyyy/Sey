af7df9000f9f5273b13d76a29264557c956d5a9a
Seyyyy <20811739+Seyyyy@users.noreply.github.com>
2024-10-30 16:10:54 +0000

    refactor: remove @eslint/compat

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
-const { fixupConfigRules } = require('@eslint/compat');
 const { FlatCompat } = require('@eslint/eslintrc');
 
 const flatCompat = new FlatCompat();
 
 module.exports = [
-  ...fixupConfigRules(flatCompat.extends('next/core-web-vitals')),
+  ...flatCompat.extends('next/core-web-vitals'),
   {
     ignores: ["node_modules", ".next", "out"],
   }
