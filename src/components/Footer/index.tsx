b64fbcf846e993d93a46a1ee73f59d708ab33efd
Seyyyy <20811739+Seyyyy@users.noreply.github.com>
2026-07-25 14:31:56 +0000

    feat: fix Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p className={styles.text}>©Sey</p>
+      <p className={styles.text}>sey.blog</p>
     </div>
   )
 }
